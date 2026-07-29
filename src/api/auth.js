7ef837679fd8dd4c93a3f53b9feb0e3baae6b195
gusqkr <hyeon05@ewha.ac.kr>
2026-07-30 01:46:49 +0900

    fix: 필드명 수정

--- src/api/auth.js
+++ src/api/auth.js
@@ -9,7 +9,7 @@ export const verifyEmailCode = (email, code) =>
   api.post("/auth/email/verify", { email, code });
 
 export const checkLoginId = (loginId) =>
-  api.get("/auth/login-id/check", { params: { "login-id": loginId } });
+  api.get("/auth/login-id/check", { params: { loginId } });
 
 export const login = (payload) => api.post("/auth/login", payload);
 
